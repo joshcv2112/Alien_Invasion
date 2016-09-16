4e93abdad17fc84f93273cb44e136b84f9bfbe04
joshcv2112 <joshcv2112@gmail.com>
2016-09-16 13:10:01 -0600

    Added ship-alien collisions (finished chapter 13)

--- settings.py
+++ settings.py
@@ -8,7 +8,10 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
         self.ship_speed_factor = 5
+        self.ship_limit = 3
 
         # Bullet settings.
         self.bullet_speed_factor = 3
